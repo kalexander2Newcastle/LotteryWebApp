f0516190b67ac1ffeef0bc5aafd252111c94cade
Kevin <k.alexander2@newcastle.ac.uk>
2022-12-02 16:44:57 +0000

    add

--- app.py
+++ app.py
@@ -13,6 +13,32 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+# Error pages
+@app.errorhandler(400)
+def bad_request(error):
+    return render_template("errors/400.html"), 400
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    return render_template("errors/403.html"), 403
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("errors/404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_service_error(error):
+    return render_template("errors/500.html"), 500
+
+
+@app.errorhandler(503)
+def service_unavailable(error):
+    return render_template("errors/503.html"), 503
+
+
 # HOME PAGE VIEW
 @app.route('/')
 def index():
@@ -24,12 +50,12 @@ def index():
 from users.views import users_blueprint
 from admin.views import admin_blueprint
 from lottery.views import lottery_blueprint
+
 #
 # # register blueprints with app
 app.register_blueprint(users_blueprint)
 app.register_blueprint(admin_blueprint)
 app.register_blueprint(lottery_blueprint)
-
 
 if __name__ == "__main__":
     app.run()
--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,35 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, ValidationError, Length, EqualTo
+
+import re
+
+
+def validate_data(self, password):
+    p = re.compile("")
+    if not p.match(password.data):
+        raise ValidationError("Password must include", p)
+
+
+def validate_phone(self, StringField):
+    p = re.compile("^d{4}[,-]\d{3}[,-]\d{4}$")
+    if not p.match(StringField.data):
+        raise ValidationError("Please enter a phone number using a valid format (0000-000-000)")
+
+
+def character_check(form, field):
+    excluded_chars = "*?!'^+%&/()=}][{$#@<>"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(f"Character {char} is not allowed")
 
 
 class RegisterForm(FlaskForm):
-    email = StringField()
-    firstname = StringField()
-    lastname = StringField()
-    phone = StringField()
-    password = PasswordField()
-    confirm_password = PasswordField()
+    email = StringField(validators=[DataRequired(), Email("Please enter a valid email address.")])
+    firstname = StringField(validators=[DataRequired(), character_check])
+    lastname = StringField(validators=[DataRequired(), character_check])
+    phone = StringField(validators=[DataRequired(), validate_phone])
+    password = PasswordField(validators=[DataRequired(), Length(min=6, max=12)])
+    confirm_password = PasswordField(validators=[DataRequired(), EqualTo('password',
+                                                                         message="Password must match field above")])
     submit = SubmitField()
